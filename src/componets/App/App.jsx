ce1e90ebe2233a6655217eb0181c3ed1f13fa250
Evan Boodoosingh <evan9496@gmail.com>
2025-09-01 22:58:06 -0400

    feat: add default clothing items and update App component to use them style: refactor App.css and Header.css for consistency fix: correct WeatherCard.css styles and ensure proper formatting style: update index.css to include fonts.css import feat: add font-face definitions for Cabinet Grotesk

--- src/componets/App/App.jsx
+++ src/componets/App/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react';
 import Header from '../Header/Header';
-import WeatherCard from '../WeatherCard/WeatherCard';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 import './App.css';
 
+import { defaultClothingItems } from '../../utils/defaultClothingItems';
 function App() {
+  const [clothingItems, setClothingItems] = useState(defaultClothingItems);
+
   return (
     <div className='app'>
       <Header />
-      <WeatherCard />
-      <Main />
+      <Main clothingItems={clothingItems} />
       <Footer />
     </div>
   );
