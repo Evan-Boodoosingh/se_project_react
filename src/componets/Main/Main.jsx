d997d7381142d1c36007696086a25f2723f48e2a
Evan Boodoosingh <evan9496@gmail.com>
2025-09-01 22:51:38 -0400

    feat: implement Main component with styling and card list

--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,5 +1,16 @@
-function Main() {
-  return <></>;
+import "./Main.css";
+import WeatherCard from '../WeatherCard/WeatherCard';
+import ItemCard from '../ItemCard/ItemCard';
+
+function Main({ clothingItems }) {
+  return <main className="main">
+   <WeatherCard />
+   <p className="main__text">Today is 75°F / you may want to wear:</p>
+<ul className="main__card-list"> {clothingItems.map((item) => {
+       return <ItemCard key={item._id} data={item} />;
+     })}
+ </ul>
+</main>;
 }
 
 export default Main;
