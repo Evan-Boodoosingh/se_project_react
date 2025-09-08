320f60377be3c93fbe822373b885a78b69e9b293
Evan Boodoosingh <evan9496@gmail.com>
2025-09-07 20:29:05 -0400

    feat: clean up code formatting and improve consistency in AddItemModal, ItemModal, and ModalWithForm components

--- src/componets/AddItemModal/AddItemModal.jsx
+++ src/componets/AddItemModal/AddItemModal.jsx
@@ -1,84 +1,77 @@
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import "../ModalWithForm/ModalWithForm.css";
 
-
 const AddItemModal = ({ isOpen, onClose, onSubmit }) => {
   return (
-  <ModalWithForm
-    title="New Garment"
-    name="add-item"
-    isOpen={isOpen}
-    onClose={onClose}
-    onSubmit={onSubmit}
-    buttonText="Add garment"
-  >
-    <label className="modal__label">
-      Name
-      <input
-        type="text"
-        className="modal__input"
-        name="item-name"
-        id="clothing-name"
-        placeholder="Name"
-        minLength="1"
-        maxLength="30"
-        required
-      />
-    </label>
-       <label className="modal__label">
-      Image
-      <input
-        type="text"
-        className="modal__input"
-        name="imageUrl"
-        id="clothing-image"
-        placeholder="Image URL"
-        required
-      />
-    </label>
-    <fieldset className="modal__fieldset">
-      <legend className="modal__legend">Select the weather type:</legend>
-      <div className="modal__radio-group">
-       <div>
-          <input
-            type="radio"
-            className="modal__radio"
-            name="weather-type"
-            value="rainy"
-            required
-          />
-           <label className="modal__label">
-          Hot
-        </label>
+    <ModalWithForm
+      title="New Garment"
+      name="add-item"
+      isOpen={isOpen}
+      onClose={onClose}
+      onSubmit={onSubmit}
+      buttonText="Add garment"
+    >
+      <label className="modal__label">
+        Name
+        <input
+          type="text"
+          className="modal__input"
+          name="item-name"
+          id="clothing-name"
+          placeholder="Name"
+          minLength="1"
+          maxLength="30"
+          required
+        />
+      </label>
+      <label className="modal__label">
+        Image
+        <input
+          type="text"
+          className="modal__input"
+          name="imageUrl"
+          id="clothing-image"
+          placeholder="Image URL"
+          required
+        />
+      </label>
+      <fieldset className="modal__fieldset">
+        <legend className="modal__legend">Select the weather type:</legend>
+        <div className="modal__radio-group">
+          <div>
+            <input
+              type="radio"
+              className="modal__radio"
+              name="weather-type"
+              value="rainy"
+              required
+            />
+            <label className="modal__label">Hot</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="modal__radio"
+              name="weather-type"
+              value="rainy"
+              required
+            />
+            <label className="modal__label">Warm</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              className="modal__radio"
+              name="weather-type"
+              value="rainy"
+              required
+            />
+            <label className="modal__label">Cold</label>
+          </div>
         </div>
-       <div>
-          <input
-            type="radio"
-            className="modal__radio"
-            name="weather-type"
-            value="rainy"
-            required
-          />
-           <label className="modal__label">
-          Warm
-        </label>
-        </div>
-              <div>
-          <input
-            type="radio"
-            className="modal__radio"
-            name="weather-type"
-            value="rainy"
-            required
-          />
-           <label className="modal__label">
-          Cold
-        </label>
-        </div>
-      </div>
-    </fieldset>
-  </ModalWithForm>
-);
-}
+      </fieldset>
+    </ModalWithForm>
+  );
+};
 
 export default AddItemModal;
--- src/componets/ItemModal/ItemModal.jsx
+++ src/componets/ItemModal/ItemModal.jsx
@@ -1,13 +1,14 @@
-import "../ItemModal/ItemModal.css"
+import "../ItemModal/ItemModal.css";
 
-
-const ItemModal = ({selectedItem, isOpen, onClose}) => (
+const ItemModal = ({ selectedItem, isOpen, onClose }) => (
   <div className={`modal ${isOpen ? "modal_open" : ""}`}>
     <div className="modal__image-container">
-      <button className="modal__close-btn modal__close-img-btn" onClick={onClose}></button>
+      <button
+        className="modal__close-btn modal__close-img-btn"
+        onClick={onClose}
+      ></button>
       <img src={selectedItem.link} className="modal__image" />
       <h2 className="modal__caption"> {selectedItem.name}</h2>
-      
     </div>
   </div>
 );
