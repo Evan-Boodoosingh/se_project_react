a1d5accfc890313ae7d2530aaefe2e6450998dea
Evan Boodoosingh <evan9496@gmail.com>
2025-09-08 01:19:30 -0400

    added weatherApi.js and constants.js

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+const coordiantes = { lat: "28.025446638640474", lon: "81.53015878569305"};
+const apiKey = "b3db5741dc986cd3ff31d13a579692b1";
+
+export { coordiantes, apiKey };
---
+++ src/utils/weatherApi.js
@@ -0,0 +1,5 @@
+import { apiKey, coordiantes } from "./constants";
+
+function getWeatherData() {
+    return fetch()
+}
