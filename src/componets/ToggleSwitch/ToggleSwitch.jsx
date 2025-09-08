efb9cb89000e12efbfeb462dd77fded95510ba39
Evan Boodoosingh <evan9496@gmail.com>
2025-09-08 19:38:41 -0400

    feat: refactor ToggleSwitch component structure and enhance styling

--- src/componets/ToggleSwitch/ToggleSwitch.jsx
+++ src/componets/ToggleSwitch/ToggleSwitch.jsx
@@ -1,13 +1,20 @@
 import "./ToggleSwitch.css";
 
 function ToggleSwitch() {
-    return (
-        <div className="toggle-switch">
-            <label className="toggle-switch__label">
-                <input type="checkbox" className="toggle-switch__checkbox" />
-            </label>
-        </div>
-    );
+  return (
+    
+      <label htmlFor="toggle-switch" className="toggle-switch">
+        <input
+          type="checkbox"
+          className="toggle-switch__checkbox"
+          id="toggle-switch"
+        />
+        <span className="toggle-switch__circle"></span>
+        <span className="toggle-switch__value toggle-switch__value_left">F</span>
+        <span className="toggle-switch__value toggle-switch__value_right">C</span>
+      </label>
+    
+  );
 }
 
 export default ToggleSwitch;
