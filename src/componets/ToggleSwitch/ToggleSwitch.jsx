595ea99d8028f154d4e8888a5155139c46802ba4
Evan Boodoosingh <evan9496@gmail.com>
2025-09-08 20:46:08 -0400

    feat: implement temperature unit toggle functionality in ToggleSwitch component

--- src/componets/ToggleSwitch/ToggleSwitch.jsx
+++ src/componets/ToggleSwitch/ToggleSwitch.jsx
@@ -1,6 +1,24 @@
 import "./ToggleSwitch.css";
+import { useEffect, useState } from "react";
 
 function ToggleSwitch() {
+
+const [tempUnit, setTempUnit] = useState("F");
+
+
+function handleChange() {
+    if (tempUnit === "F") {
+        setTempUnit("C");
+    } else {
+        setTempUnit("F");
+    }
+}
+
+
+// useEffect(() => {
+//     console.log("Temperature unit changed to:", tempUnit);
+// });
+
   return (
     
       <label htmlFor="toggle-switch" className="toggle-switch">
@@ -8,6 +26,7 @@ function ToggleSwitch() {
           type="checkbox"
           className="toggle-switch__checkbox"
           id="toggle-switch"
+          onChange={handleChange}
         />
         <span className="toggle-switch__circle"></span>
         <span className="toggle-switch__value toggle-switch__value_left">F</span>
