6e17cbdf7305963670affa12ed5b49e7220864b7
Evan Boodoosingh <evan9496@gmail.com>
2025-09-09 02:32:35 -0400

    refactor: improve code formatting and readability in Main component

--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,25 +1,37 @@
 import "./Main.css";
 import { useContext } from "react";
 import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
-import WeatherCard from '../WeatherCard/WeatherCard';
-import ItemCard from '../ItemCard/ItemCard';
+import WeatherCard from "../WeatherCard/WeatherCard";
+import ItemCard from "../ItemCard/ItemCard";
 
 function Main({ clothingItems, onViewItem, weatherData }) {
   const { currentTempUnit } = useContext(CurrentTemperatureUnitContext);
 
   // Convert F to C if needed
   const tempF = weatherData.temp.F;
-  const tempC = Math.round((tempF - 32) * 5 / 9);
+  const tempC = Math.round(((tempF - 32) * 5) / 9);
   const displayTemp = currentTempUnit === "C" ? tempC : tempF;
 
-  return <main className="main">
-   <WeatherCard weatherData={weatherData} />
-   <p className="main__text">Today is {displayTemp}°{currentTempUnit} / you may want to wear:</p>
-<ul className="main__card-list"> {clothingItems.map((item) => {
-       return <ItemCard key={item._id} data={item} onClick={() => onViewItem(item)} />;
-     })}
- </ul>
-</main>;
+  return (
+    <main className="main">
+      <WeatherCard weatherData={weatherData} />
+      <p className="main__text">
+        Today is {displayTemp}°{currentTempUnit} / you may want to wear:
+      </p>
+      <ul className="main__card-list">
+        {" "}
+        {clothingItems.map((item) => {
+          return (
+            <ItemCard
+              key={item._id}
+              data={item}
+              onClick={() => onViewItem(item)}
+            />
+          );
+        })}
+      </ul>
+    </main>
+  );
 }
 
 export default Main;
