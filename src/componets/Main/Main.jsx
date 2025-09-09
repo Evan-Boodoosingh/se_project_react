f99e6d0452a7b524ddc29157b756f1c41508e924
Evan Boodoosingh <evan9496@gmail.com>
2025-09-09 01:55:19 -0400

    feat: integrate temperature unit context into Main component

--- src/componets/Main/Main.jsx
+++ src/componets/Main/Main.jsx
@@ -1,11 +1,15 @@
 import "./Main.css";
+import { useContext } from "react";
+import CurrentTemperatureUnitContext from "../../contexts/CurrentTemperatureUnitContext";
 import WeatherCard from '../WeatherCard/WeatherCard';
 import ItemCard from '../ItemCard/ItemCard';
 
 function Main({ clothingItems, onViewItem, weatherData }) {
+  const { currentTempUnit } = useContext(CurrentTemperatureUnitContext);
+
   return <main className="main">
    <WeatherCard weatherData={weatherData} />
-   <p className="main__text">Today is {weatherData.temp}°F / you may want to wear:</p>
+   <p className="main__text">Today is {weatherData.temp}°{currentTempUnit} / you may want to wear:</p>
 <ul className="main__card-list"> {clothingItems.map((item) => {
        return <ItemCard key={item._id} data={item} onClick={() => onViewItem(item)} />;
      })}
