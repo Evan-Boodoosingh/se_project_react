f2ea0ee22158fd00b079074deb7e071ba30ff375
Evan Boodoosingh <evan9496@gmail.com>
2025-09-18 18:20:37 -0400

    feat: enhance DeleteModal with improved styling and confirmation message; add grey.svg asset

--- src/components/DeleteModal/DeleteModal.jsx
+++ src/components/DeleteModal/DeleteModal.jsx
@@ -3,15 +3,19 @@ import "./DeleteModal.css";
 const DeleteModal = ({ handleDelete, isOpen, onClose }) => (
   <div className={`modal ${isOpen ? "modal_open" : ""}`}>
     <div className="delete-modal__delete-container">
+      <div className="delete-modal_container-inner">
+      <p className="delete-modal__caption">Are you sure you want to delete this item?
+This action is irreversible.</p>
       <button
         className="delete-modal__close-btn delete-modal__close-img-btn"
         onClick={onClose}
       ></button>
-      <div>
+      <div className="delete-modal__button-container">
         <button onClick={handleDelete} className="delete-modal__btn-delete">
-          Delete item
+          Yes, delete item
         </button>
-        <button className="delete-modal__btn-cancel"></button>
+        <button className="delete-modal__btn-cancel" onClick={onClose}>Cancel</button>
+        </div>
       </div>
     </div>
   </div>
